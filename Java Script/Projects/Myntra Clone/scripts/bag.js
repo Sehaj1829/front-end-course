fbe6c973a05c24d8430fc1ff8a516e54b118d694
Sehaj1829 <sehaj1829.be21@chitkara.edu.in>
2025-09-30 13:26:12 +0530

    auto commit

--- Java Script/Projects/Myntra Clone/scripts/bag.js
+++ Java Script/Projects/Myntra Clone/scripts/bag.js
@@ -54,15 +54,18 @@ function displayBagSummary()
 
 function loadBagItemObjects() 
 {
-  console.log(bagItems);
-  bagItemObjects = bagItems.map(itemId => {
-    for (let i = 0; i < items.length; i++) {
-      if (itemId == items[i].id) {
-        return items[i];
-      }
-    }
-  });
-  console.log(bagItemObjects);
+    console.log(bagItems);
+    bagItemObjects = bagItems.map(itemId => 
+    {
+        for (let i = 0; i < items.length; i++) 
+        {
+            if (itemId == items[i].id) 
+            {
+                return items[i];
+            }
+        }
+    });
+    console.log(bagItemObjects);
 }
 
 function displayBagItems() 
